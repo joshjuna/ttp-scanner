a8de7977b27ea051cce78d4ab3f3424e86a9437f
Anuj Joshi <januj@amazon.com>
2024-07-06 09:23:20 -0700

    adding ttp.sh

---
+++ ttp.sh
@@ -0,0 +1,12 @@
+echo "Executing at `date` for $1" | tee ttp.out
+result=$(curl -s 'https://ttp.cbp.dhs.gov/schedulerapi/locations/'$1'/slots?startTimestamp=2024-09-07T00:00&endTimestamp=2024-10-10T00%3A00')
+#echo $result | jq
+activeResult=$(jq -n --argjson arr "$result" '$arr | flatten | map(select(.active? and .active != 0))')
+echo $activeResult | jq | tee ttp.out
+length=$(jq -n --argjson arr "$activeResult" '$arr | length')
+#echo $length
+
+if [ "$length" -gt 0 ]; then
+  echo $activeResult | jq  >> ttp.json
+  curl 'https://api-v2.voicemonkey.io/trigger?token=e950d995ecc273aede5ecbdecfbedeb9_0a0b4df950e95ad48f9fc15fd0d1b2fe&device=high-temp';
+fi
